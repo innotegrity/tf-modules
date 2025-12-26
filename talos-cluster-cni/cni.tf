98dc397048c39db8f4dd50c219cbddba8337aa0e
Josh Hogle <josh@innotegrity.com>
2025-12-25 19:04:58 -0500

    Fixed value for Helm

--- talos-cluster-cni/cni.tf
+++ talos-cluster-cni/cni.tf
@@ -63,7 +63,7 @@ resource "helm_release" "cilium_cni" {
     },
     {
       name  = "gatewayAPI.gatewayClass.create"
-      type  = string
+      type  = "string"
       value = "true"
     },
     {
