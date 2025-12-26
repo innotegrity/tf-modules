87dcb3cbd77a9d6c8ba3282c0aff4b2df798c0c2
Josh Hogle <josh@innotegrity.com>
2025-12-25 20:11:27 -0500

    Fixed issue with yamldecode

--- talos-cluster-cni/locals.tf
+++ talos-cluster-cni/locals.tf
@@ -2,9 +2,9 @@ locals {
   gateway_crds_url  = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v${var.gateway_crds_version}/${var.gateway_crds_experimental ? "experimental" : "standard"}-install.yaml"
   gateway_crds_docs = split("---", data.http.gateway_crds.response_body)
   gateway_crds_decoded_docs = [
-    for doc in local.gateway_crds_docs : try(yamldecode(doc), null)
+    for doc in local.gateway_crds_docs : try(yamldecode(doc), {})
   ]
   gateway_crds_manifests = [
-    for doc in local.gateway_crds_decoded_docs : doc if doc != null
+    for doc in local.gateway_crds_decoded_docs : doc if length(doc) > 0
   ]
 }
